18bb092319b3d542edf17076482dbadc45c16050
William Dubuc <william.dubuc01@gmail.com>
2026-03-04 11:47:34 -0500

    Split avec terrain

--- gamewidget.h
+++ gamewidget.h
@@ -12,9 +12,9 @@
 #include "line.h"
 #include <QTextDocument>
 #include <QDebug>
+#include "terrain.h"
 
 constexpr static int FRAME_RATE = 180;
-constexpr static int N_LINES = 2000;
 
 class GameWidget: public QWidget
 {
@@ -33,26 +33,13 @@ private slots:
 
 private:
     Player player;
-
-    QVector<Line> lines;
+    Terrain terrain;
 
     int frameCount = 0;
     int currentFps = 0;
     QElapsedTimer timer;
     QElapsedTimer fpsTimer;
     QLinearGradient sunset;
-    QPixmap testSprite;
-
-    /**
-     * @brief drawQuad Fait un quad sur l'écran
-     * @param x1 x du milieu de la ligne du haut
-     * @param y1 y de la ligne du haut
-     * @param w1 demi largeur de la ligne du haut
-     * @param x2 x du milieu de la ligne du bas
-     * @param y2 y de la ligne du bas
-     * @param w2 demi largeur de la ligne du bas
-     */
-    void drawQuad(QPainter &painter, QColor color, int x1, int y1, int w1, int x2, int y2, int w2);
 };
 
 #endif // GAMEWIDGET_H
---
+++ terrain.h
@@ -0,0 +1,39 @@
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+#include <QVector>
+#include <QPainter>
+#include "line.h"
+#include "player.h"
+
+class Terrain
+{
+public:
+    Terrain();
+
+    constexpr static int N_LINES = 2000;
+
+    void render(QPainter &painter, Player &player, int screenWidth, int screenHeight);
+    void generateTerrain();
+
+    Line& getLine(int index);
+    const Line& getLine(int index) const;
+    int getTotalLines() const;
+
+private:
+    QVector<Line> lines;
+    QPixmap testSprite;
+
+    /**
+     * @brief drawQuad Fait un quad sur l'écran
+     * @param x1 x du milieu de la ligne du haut
+     * @param y1 y de la ligne du haut
+     * @param w1 demi largeur de la ligne du haut
+     * @param x2 x du milieu de la ligne du bas
+     * @param y2 y de la ligne du bas
+     * @param w2 demi largeur de la ligne du bas
+     */
+    void drawQuad(QPainter &painter, QColor color, int x1, int y1, int w1, int x2, int y2, int w2);
+};
+
+#endif // TERRAIN_H
